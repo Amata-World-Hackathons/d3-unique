453039c96717918e08d79656bae0b9789a129e88
Bryan Yap <bryan@amata.world>
2022-07-13 06:49:25 +0100

    fix deploys

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 const nextConfig = {
+  basePath,
+  assetPrefix: basePath,
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
